35771a9ad3488883dac8f918b63ec8731507e2c6
Hallworld <prishedko.ilya@yandex.ru>
2024-07-20 02:34:51 +0400

    Update saveCourt for new Court entity Add getCourtByAddress

--- BasketCourtsApp/src/main/kotlin/halloworld/BasketCourtsApp/service/CourtService.kt
+++ BasketCourtsApp/src/main/kotlin/halloworld/BasketCourtsApp/service/CourtService.kt
@@ -2,6 +2,7 @@ package halloworld.BasketCourtsApp.service
 
 import halloworld.BasketCourtsApp.entity.Address
 import halloworld.BasketCourtsApp.entity.Court
+import halloworld.BasketCourtsApp.entity.CourtName
 import halloworld.BasketCourtsApp.entity.enums.SurfaceType
 import halloworld.BasketCourtsApp.repository.AddressRepository
 import halloworld.BasketCourtsApp.repository.CourtRepository
@@ -26,11 +27,18 @@ class CourtService(
         return courtRepository.updateSurfaceTypeById(surface, id)
     }
 
-    fun saveCourt(city: String, street: String, number: String,
-                  surface: String, height: Int = 0): Court {
+    fun saveCourt(
+        city: String, street: String, number: String,
+                  surface: String, height: Int = 0, countRims: Int = 0,
+                  names: Set<CourtName> = setOf()
+    ): Court {
         val address = addressRepository.save(Address(city, street, number))
         val surfaceType = SurfaceType.valueOf(surface)
-        val court = Court(address, surfaceType, height)
+        val court = Court(address, surfaceType, height, countRims, names)
         return courtRepository.save(court)
+    }
+
+    fun getCourtByAddress(city: String, street: String, number: String): Court {
+        return courtRepository.getByAddress(city, street, number)
     }
 }
