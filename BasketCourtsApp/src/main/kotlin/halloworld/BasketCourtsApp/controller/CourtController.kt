cf019e77cb2c48a2103c39e97e49040a78b978bd
Hallworld <prishedko.ilya@yandex.ru>
2024-07-20 02:56:25 +0400

    Add some api

--- BasketCourtsApp/src/main/kotlin/halloworld/BasketCourtsApp/controller/CourtController.kt
+++ BasketCourtsApp/src/main/kotlin/halloworld/BasketCourtsApp/controller/CourtController.kt
@@ -1,22 +1,25 @@
 package halloworld.BasketCourtsApp.controller
 
-import halloworld.BasketCourtsApp.entity.Address
 import halloworld.BasketCourtsApp.entity.Court
-import halloworld.BasketCourtsApp.repository.AddressRepository
 import halloworld.BasketCourtsApp.service.CourtService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CourtController(private val courtService: CourtService) {
 
-    @GetMapping("/city/{city}")
+    @GetMapping("/{city}")
     fun getCityCourts(@PathVariable city: String): Set<Court> {
         return courtService.getCourtsByCity(city)
     }
+
+    @GetMapping("/{city}", params = ["city", "street", "number"])
+    fun getCourt(@PathVariable city: String, street: String, number: String): Court {
+        return courtService.getCourtByAddress(city, street, number)
+    }
+
     @PostMapping("/court")
     fun saveCourt(city: String, street: String, number: String,
                     surface: String, height: Int = 0): Court {
