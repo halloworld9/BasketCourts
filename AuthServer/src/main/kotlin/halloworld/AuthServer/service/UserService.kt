341f2e0a5d27bf946e9a1afae8d9518d90aee8f0
Halloworld <prishedko.ilya@yandex.ru>
2024-08-25 01:18:41 +0400

    implement UserDetailsManager in UserService for registration

--- AuthServer/src/main/kotlin/halloworld/AuthServer/service/UserService.kt
+++ AuthServer/src/main/kotlin/halloworld/AuthServer/service/UserService.kt
@@ -3,28 +3,27 @@ package halloworld.AuthServer.service
 import halloworld.AuthServer.entity.Role
 import halloworld.AuthServer.entity.User
 import halloworld.AuthServer.repository.UserRepository
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
-import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.provisioning.UserDetailsManager
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
 class UserService(private val userRepository: UserRepository, private val encoder: PasswordEncoder) :
-    UserDetailsService {
+    UserDetailsManager {
 
     init {
-        if (!userExistsWithName("admin")) saveUser(User("admin@yandex.ru", "admin", "12345", Date(), Role.ADMIN))
-        if (!userExistsWithName("user")) saveUser(User("eshkere@yandex.ru", "user", "12345", Date(), Role.USER))
+        if (!userExists("admin@yandex.ru")) saveUser(User("admin@yandex.ru", "admin", "12345", Date(), Role.ADMIN))
+        if (!userExists("eshkere@yandex.ru")) saveUser(User("eshkere@yandex.ru", "user", "12345", Date(), Role.USER))
     }
 
     fun findUserById(id: Long): Optional<User> {
         return userRepository.findById(id)
     }
 
-    fun findUserByEmail(email: String): Optional<User> {
-        return userRepository.findByEmail(email)
-    }
 
     fun findUserByUsername(username: String): Optional<User> {
         return userRepository.findByUsername(username)
@@ -40,7 +39,32 @@ class UserService(private val userRepository: UserRepository, private val encode
         return userRepository.findByEmail(username).get()
     }
 
-    fun userExistsWithName(username: String): Boolean {
-        return userRepository.findByUsername(username).isPresent
+    override fun createUser(user: UserDetails) {
+        user as User
+        user.password = encoder.encode(user.password)
+        userRepository.save(user)
     }
+
+    override fun updateUser(user: UserDetails) {
+        user as User
+        user.run { userRepository.updateUser(email, name, role, password) }
+    }
+
+    override fun deleteUser(email: String) {
+        userRepository.deleteByEmail(email)
+    }
+
+    override fun changePassword(oldPassword: String, newPassword: String) {
+        val currentUser: Authentication = SecurityContextHolder.getContext().authentication
+            ?: throw org.springframework.security.access.AccessDeniedException(
+                "Can't change password as no Authentication object found in context " + "for current user."
+            )
+        userRepository.updatePasswordByEmail(newPassword, currentUser.name)
+
+    }
+
+    override fun userExists(email: String): Boolean {
+        return userRepository.existsByEmail(email)
+    }
+
 }
