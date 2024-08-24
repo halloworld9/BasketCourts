037e44577281d1986ccf1ff06346931ca8265573
Halloworld <prishedko.ilya@yandex.ru>
2024-08-25 01:19:06 +0400

    Add UserRepository for service

--- AuthServer/src/main/kotlin/halloworld/AuthServer/repository/UserRepository.kt
+++ AuthServer/src/main/kotlin/halloworld/AuthServer/repository/UserRepository.kt
@@ -1,9 +1,12 @@
 package halloworld.AuthServer.repository
 
+import halloworld.AuthServer.entity.Role
 import halloworld.AuthServer.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 import java.util.*
 
 @Repository
@@ -22,5 +25,27 @@ interface UserRepository: JpaRepository<User, Long> {
     fun findByUsername(username: String): Optional<User>
 
 
+    @Transactional
+    @Modifying
+    @Query("delete from User u where upper(u.email) = upper(:email)")
+    fun deleteByEmail(email: String)
 
+
+    @Query("select (count(u) > 0) from User u where upper(u.email) = upper(:email)")
+    fun existsByEmail(email: String): Boolean
+
+
+    @Transactional
+    @Modifying
+    @Query(
+        """update User u set u.name = :name, u.password = :password, u.role = :role
+    where upper(u.email) = upper(:email)"""
+    )
+    fun updateUser(name: String, password: String, role: Role, email: String)
+
+
+    @Transactional
+    @Modifying
+    @Query("update User u set u.password = :password where upper(u.email) = upper(:email)")
+    fun updatePasswordByEmail(password: String, email: String): Int
 }
